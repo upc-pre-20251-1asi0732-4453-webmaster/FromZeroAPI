0b4173b572ca473b6991f77ae770b56580b8d56b
Jair Velasquez <106536136+Purak189@users.noreply.github.com>
2024-11-05 22:23:52 -0500

    feat: inject data to pro. languages and frameworks

--- src/main/java/com/fromzero/backend/FromZeroBackendApplication.java
+++ src/main/java/com/fromzero/backend/FromZeroBackendApplication.java
@@ -1,5 +1,11 @@
 package com.fromzero.backend;
 
+import com.fromzero.backend.projects.domain.model.aggregates.Framework;
+import com.fromzero.backend.projects.domain.model.aggregates.ProgrammingLanguage;
+import com.fromzero.backend.projects.infrastructure.persistence.jpa.repositories.FrameworksRepository;
+import com.fromzero.backend.projects.infrastructure.persistence.jpa.repositories.ProgrammingLanguagesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,10 +13,29 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 
-public class FromZeroBackendApplication {
+public class FromZeroBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private FrameworksRepository frameworkRepository;
+
+	@Autowired
+	private ProgrammingLanguagesRepository programmingLanguageRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FromZeroBackendApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		frameworkRepository.save(new Framework("Spring Boot"));
+		frameworkRepository.save(new Framework("Vue..js"));
+		frameworkRepository.save(new Framework("Angular"));
+		frameworkRepository.save(new Framework("Nest.js"));
+		frameworkRepository.save(new Framework("Net Core"));
+		programmingLanguageRepository.save(new ProgrammingLanguage("Java"));
+		programmingLanguageRepository.save(new ProgrammingLanguage("Python"));
+		programmingLanguageRepository.save(new ProgrammingLanguage("JavaScript"));
+		programmingLanguageRepository.save(new ProgrammingLanguage("C#"));
+		programmingLanguageRepository.save(new ProgrammingLanguage("Ruby"));
+	}
 }
