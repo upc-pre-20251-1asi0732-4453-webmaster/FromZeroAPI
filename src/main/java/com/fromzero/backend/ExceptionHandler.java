34d47dfb7e9c37d27f61a3bbabe268ca0106b720
Sam <samirajetzabel@gmail.com>
2025-05-07 23:36:29 -0500

    feat: add fileUrl attribute in deliverable model

--- src/main/java/com/fromzero/backend/ExceptionHandler.java
+++ src/main/java/com/fromzero/backend/ExceptionHandler.java
@@ -3,7 +3,6 @@ package com.fromzero.backend;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
@@ -11,9 +10,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class ExceptionBerraco {
+public class ExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class) // Puedes cambiar a Exception.class si quieres manejar todas las excepciones
+    @org.springframework.web.bind.annotation.ExceptionHandler(RuntimeException.class) // Puedes cambiar a Exception.class si quieres manejar todas las excepciones
     public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex, WebRequest request) {
         Map<String, Object> errorDetails = new HashMap<>();
         errorDetails.put("timestamp", LocalDateTime.now()); // Fecha del error
