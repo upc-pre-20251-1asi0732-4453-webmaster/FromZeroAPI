e31d60abca908398ca10dbfc7a9e435bde6a81dc
Jair Velasquez <106536136+Purak189@users.noreply.github.com>
2025-05-11 13:40:50 -0500

    feat: devs cans see proyects that they are candidates

--- src/main/java/com/fromzero/backend/projects/infrastructure/persistence/jpa/repositories/ProjectRepository.java
+++ src/main/java/com/fromzero/backend/projects/infrastructure/persistence/jpa/repositories/ProjectRepository.java
@@ -6,6 +6,7 @@ import com.fromzero.backend.projects.domain.valueobjects.ProjectState;
 import com.fromzero.backend.user.domain.model.aggregates.Developer;
 import com.fromzero.backend.user.domain.model.aggregates.Enterprise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +17,6 @@ public interface ProjectRepository extends JpaRepository<Project,Long> {
     List<Project> findAllByDeveloper(Developer developer);
     List<Project> findAllByEnterprise(Enterprise enterprise);
 
+    @Query("SELECT p FROM Project p WHERE p.developer = :developer OR :developer MEMBER OF p.candidates")
+    List<Project> findAllByDeveloperOrCandidate(Developer developer);
 }
