8696c99373dfef69a9c33fbf0325ccd68b87cc40
HamesEl <u202212684@upc.edu.pe>
2025-05-14 18:53:17 -0500

    fix: update all for deploy

--- src/main/java/com/fromzero/backend/FromZeroBackendApplication.java
+++ src/main/java/com/fromzero/backend/FromZeroBackendApplication.java
@@ -8,15 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@ComponentScan(basePackages = {
-		"com.fromzero.backend",
-		"com.fromzero.backend.iam.infrastructure"
-})
+
 public class FromZeroBackendApplication implements CommandLineRunner {
 
 	@Autowired
